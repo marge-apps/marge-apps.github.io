a8b8685f58e832e5c6fcf09f9ae9ba13a15590ff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-07 10:53:59 +0000

    Deploy marge-apps/marge-apps.github.io to github.com/marge-apps/marge-apps.github.io.git:gh-pages

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-43612d304b7f8894312d.js","b980335ad6de0ddfb603b79a6f7b7aeb"],["/commons-6e82eaae3f56c620a9ec.js","3d2ca461d4c6f4791789095b060364f9"],["/index.html","66ddd5f6d82799c8c45175448998dfb9"],["/offline-plugin-app-shell-fallback/index.html","c1bb6a6125cc954707d5bb8373abf54b"]];
+var precacheConfig = [["/app-0a04c08345a753678f75.js","44385b71ec2cb150d30d9fe70d6a2f32"],["/commons-6e82eaae3f56c620a9ec.js","9740518286f608afe63b4cee2f472850"],["/index.html","453bd367c57b4679c50130c6ebc8e001"],["/offline-plugin-app-shell-fallback/index.html","d1bedc6f1459acac27bb034054a64e48"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
